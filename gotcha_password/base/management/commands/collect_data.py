a0eb8b099ccf31c4ce527b7f8c617b7c650c3b48
Brandon Chinn <brandonchinn178@gmail.com>
2016-11-28 18:09:07 -0800

    remove redis

--- gotcha_password/base/management/commands/collect_data.py
+++ gotcha_password/base/management/commands/collect_data.py
@@ -2,13 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.files.storage import default_storage
 from django.db.models import Min, Max, Avg, F
 from django.utils.timezone import localtime
+from django.conf import settings
 
 import os, sys
 from xlwt.Workbook import Workbook
 from datetime import datetime
 
 from base.models import *
-from base.redis_utils import REDIS_QUEUE
 
 class ExcelSheet(object):
     """
@@ -36,89 +36,82 @@ class ExcelSheet(object):
         for row in rows:
             self.add_row(row)
 
-def run_collect_data(log_progress):
-    def log(msg='', newline=True):
-        if log_progress:
+class Command(BaseCommand):
+    def log(self, msg='', newline=True):
+        if not settings.IS_HEROKU:
             end = '\n' if newline else ''
             sys.stdout.write(msg + end)
             sys.stdout.flush()
 
-    # Save a file with summary statistics
-    log('Saving summary stats...')
-    percent_correct = 1.0 * F('correct_images') / F('user__num_images')
-    percentages = LoginAttempt.objects.filter(right_password=True
-        ).annotate(percent_correct=percent_correct
-        ).order_by('percent_correct'
-        ).values_list('percent_correct', flat=True
-        )
-    total = len(percentages)
-    middle = total / 2 - 1
-    if total % 2 == 0:
-        median = sum(percentages[middle:middle+2]) / 2
-    else:
-        median = percentages[middle]
-
-    with default_storage.open('summary.txt', 'w') as f:
-        f.write('\n'.join([
-            'Total accounts: %d' % User.objects.count(),
-            'Total login attempts: %d' % LoginAttempt.objects.count(),
-            'Total login attempts with invalid passwords: %d' % LoginAttempt.objects.filter(right_password=False).count(),
-            'Min percentage: %f' % percentages[0],
-            'Max percentage: %f' % percentages[total - 1],
-            'Average percentage: %f' % (sum(percentages) / float(total)),
-            'Median percentage: %f' % median,
-        ]))
-
-    # Save a file with all the data as an Excel file
-    log('Saving all data...')
-    workbook = Workbook()
-
-    users_sheet = ExcelSheet(workbook, 'Users')
-    fields = ['timestamp', 'username', 'email', 'num_images']
-    values = User.objects.values_list(*fields)
-    users_sheet.write_headers(fields)
-    users_sheet.add_rows(values)
-
-    logins_sheet = ExcelSheet(workbook, 'Login Attempts')
-    logins_sheet.write_headers(['Timestamp', 'Username', 'Right password?', '# correct images', '# total images', 'Percent correct'])
-    images_accuracy = ExcelSheet(workbook, 'NumImages_Accuracy')
-    images_accuracy.write_headers(['Number of Images', 'Percent Correct'])
-    check_sheet = ExcelSheet(workbook, 'Check')
-    check_sheet.write_headers(['Number of Images', 'Accuracy Threshold', 'Time', 'Right password'])
-    crack_sheet = ExcelSheet(workbook, 'Crack')
-    crack_sheet.write_headers(['Number of Images', 'Accuracy Threshold', 'Time'])
-    for login in LoginAttempt.objects.annotate(percent_correct=percent_correct):
-        num_images = login.user.num_images
-
-        logins_sheet.add_row([
-            login.timestamp,
-            login.user.username,
-            login.right_password,
-            login.correct_images,
-            num_images,
-            login.percent_correct
-        ])
-        if login.right_password:
-            # only run accuracy for logins with the right password
-            images_accuracy.add_row([num_images, login.percent_correct])
-
-        try:
-            benchmarks = login.get_benchmarks()
-        except:
-            continue
-
-        for threshold, time in enumerate(benchmarks['check']):
-            check_sheet.add_row([num_images, threshold, time, login.right_password])
-        for threshold, time in enumerate(benchmarks['crack']):
-            crack_sheet.add_row([num_images, threshold, time])
-
-    with default_storage.open('data.xls', 'w+') as f:
-        workbook.save(f)
-
-class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        if REDIS_QUEUE is not None:
-            REDIS_QUEUE.enqueue(run_collect_data, log_progress=False)
-            print 'Collecting data queued.'
+        # Save a file with summary statistics
+        self.log('Saving summary stats...')
+        percent_correct = 1.0 * F('correct_images') / F('user__num_images')
+        percentages = LoginAttempt.objects.filter(right_password=True
+            ).annotate(percent_correct=percent_correct
+            ).order_by('percent_correct'
+            ).values_list('percent_correct', flat=True
+            )
+        total = len(percentages)
+        middle = total / 2 - 1
+        if total % 2 == 0:
+            median = sum(percentages[middle:middle+2]) / 2
         else:
-            run_collect_data(log_progress=True)
+            median = percentages[middle]
+
+        with default_storage.open('summary.txt', 'w') as f:
+            f.write('\n'.join([
+                'Total accounts: %d' % User.objects.count(),
+                'Total login attempts: %d' % LoginAttempt.objects.count(),
+                'Total login attempts with invalid passwords: %d' % LoginAttempt.objects.filter(right_password=False).count(),
+                'Min percentage: %f' % percentages[0],
+                'Max percentage: %f' % percentages[total - 1],
+                'Average percentage: %f' % (sum(percentages) / float(total)),
+                'Median percentage: %f' % median,
+            ]))
+
+        # Save a file with all the data as an Excel file
+        self.log('Saving all data...')
+        workbook = Workbook()
+
+        users_sheet = ExcelSheet(workbook, 'Users')
+        fields = ['timestamp', 'username', 'email', 'num_images']
+        values = User.objects.values_list(*fields)
+        users_sheet.write_headers(fields)
+        users_sheet.add_rows(values)
+
+        logins_sheet = ExcelSheet(workbook, 'Login Attempts')
+        logins_sheet.write_headers(['Timestamp', 'Username', 'Right password?', '# correct images', '# total images', 'Percent correct'])
+        images_accuracy = ExcelSheet(workbook, 'NumImages_Accuracy')
+        images_accuracy.write_headers(['Number of Images', 'Percent Correct'])
+        check_sheet = ExcelSheet(workbook, 'Check')
+        check_sheet.write_headers(['Number of Images', 'Accuracy Threshold', 'Time', 'Right password'])
+        crack_sheet = ExcelSheet(workbook, 'Crack')
+        crack_sheet.write_headers(['Number of Images', 'Accuracy Threshold', 'Time'])
+        for login in LoginAttempt.objects.annotate(percent_correct=percent_correct):
+            num_images = login.user.num_images
+
+            logins_sheet.add_row([
+                login.timestamp,
+                login.user.username,
+                login.right_password,
+                login.correct_images,
+                num_images,
+                login.percent_correct
+            ])
+            if login.right_password:
+                # only run accuracy for logins with the right password
+                images_accuracy.add_row([num_images, login.percent_correct])
+
+            try:
+                benchmarks = login.get_benchmarks()
+            except:
+                continue
+
+            for threshold, time in enumerate(benchmarks['check']):
+                check_sheet.add_row([num_images, threshold, time, login.right_password])
+            for threshold, time in enumerate(benchmarks['crack']):
+                crack_sheet.add_row([num_images, threshold, time])
+
+        with default_storage.open('data.xls', 'w+') as f:
+            workbook.save(f)
--- gotcha_password/base/management/commands/run_benchmarks.py
+++ gotcha_password/base/management/commands/run_benchmarks.py
@@ -1,16 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 
 from base.models import *
-from base.redis_utils import REDIS_QUEUE
 from base.utils import run_benchmarks
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        login_attempts = LoginAttempt.objects.filter(benchmarks='')
-        if REDIS_QUEUE is not None:
-            for login_attempt in login_attempts:
-                REDIS_QUEUE.enqueue(run_benchmarks, login_attempt, log_progress=False)
-            print 'Benchmarks queued.'
-        else:
-            for login_attempt in login_attempts:
-                run_benchmarks(login_attempt, log_progress=True)
+        for login_attempt in LoginAttempt.objects.filter(benchmarks=''):
+            run_benchmarks(login_attempt, log_progress=not settings.IS_HEROKU)
